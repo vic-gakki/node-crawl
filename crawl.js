278320b6ac5a203a46a6a80638015775fe470c8a
victor <1041203139@qq.com>
2019-05-23 02:48:11 +0800

    add

--- crawl.js
+++ crawl.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const https = require('https')
 const cheerio = require('cheerio')
+const ejs = require('ejs')
+const path = require('path')
+const nodemailer = require('nodemailer')
 
 function crawlHTML(url){
 	return new Promise((resolve, reject) => {
@@ -20,14 +23,20 @@ function crawlHTML(url){
 		})
 	})
 }
-
-// crawlHTML('http://www.lssdjt.com').then(parseTodayonHistory).catch(err => {
-
-// })
-// crawlHTML('http://wufazhuce.com').then(parseTheOne).catch(err => {
-
-// })
-crawlHTML('https://jikipedia.com').then(parseJikipedia).catch(err => {
+function
+let parser = [parseTodayonHistory, parseTheOne, parseJikipedia],
+		promises = ['http://www.lssdjt.com', 'http://wufazhuce.com', 'https://jikipedia.com'].map((item, index) => {
+			return crawlHTML(item).then(res => {
+				return parser[index](res)
+			})
+		})
+Promise.all(promises).then(res => {
+	let html = ejs.renderFile(path.resolve(__dirname, 'email.ejs'), {
+		history: res[0],
+		theOne: res[1],
+		jikipedia: res[2]
+	})
+}).catch(err => {
 	console.log(err)
 })
 
@@ -38,17 +47,17 @@ function parseTodayonHistory($){
 				desc: []
 			}
 	data.date = $('div.info').text()
-	$('div.main li').each((index, element) => {
+	$('ul.list li').each((index, element) => {
 		let anchor = $(element).children('a'),
 				ref = anchor.attr('rel'),
 				text = anchor.text()
 		if(ref){
-			illus.push({
+			data.illus.push({
 				img: ref,
 				text
 			})
 		}else {
-			desc.push(text)
+			data.desc.push(text)
 		}
 	})
 	return data
@@ -74,7 +83,6 @@ function parseJikipedia($){
 			title, content
 	title = selected.find('.title').text().trim()
 	content = selected.find('.brax-render').text()
-	console.log(title, content)
 	return {
 		title,
 		content
